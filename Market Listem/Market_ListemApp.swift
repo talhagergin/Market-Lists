d149b14a8b7ac87b49c630f9105e89f83c617ca5
Talha Gergin <talhagergin6@gmail.com>
2026-09-04 15:04:55 +0300

    Initial Commit

---
+++ Market Listem/Market_ListemApp.swift
@@ -0,0 +1,17 @@
+//
+//  Market_ListemApp.swift
+//  Market Listem
+//
+//  Created by Talha Gergin on 4.09.2026.
+//
+
+import SwiftUI
+
+@main
+struct Market_ListemApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
